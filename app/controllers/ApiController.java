21580d565d63a7a2071ba907076a20b6cc3e992d
David Gubler <david@kabelverhau.ch>
2026-08-31 21:11:17 +0200

    fix: Product names for API suggestions

--- app/controllers/ApiController.java
+++ app/controllers/ApiController.java
@@ -83,7 +83,7 @@ public class ApiController extends Controller {
         public String toString() {
             String start = realizedPass.getTrip().getBegins().getName();
             String end = realizedPass.getTrip().getEnds().getName();
-            String shortName = realizedPass.getTrip().getRoute().getShortName();
+            String shortName = realizedPass.getTrip().getProduct();
             String tripShortName = realizedPass.getTrip().getTrip().getTrainNr();
             String late = "";
             if (getLateSeconds() >= 300 && getLateSeconds() <= 600) {
--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 sbt clean; sbt dist || exit 1
-scp target/universal/railinfo-1.0-SNAPSHOT.zip railinfo.kabelverhau.ch: || exit 1
-ssh railinfo.kabelverhau.ch 'sudo systemctl stop railinfo; rm -rf railinfo-1.0-SNAPSHOT; unzip railinfo-1.0-SNAPSHOT.zip; sudo systemctl start railinfo' || exit 1
+#scp -o ProxyJump=david@backup0.kabelverhau.ch target/universal/railinfo-1.0-SNAPSHOT.zip railinfo.kabelverhau.ch: || exit 1
+#ssh railinfo.kabelverhau.ch -J backup0.kabelverhau.ch 'sudo systemctl stop railinfo; rm -rf railinfo-1.0-SNAPSHOT; unzip railinfo-1.0-SNAPSHOT.zip; sudo systemctl start railinfo' || exit 1
 
 host="railinfo.kabelverhau.ch"
-rsync --rsync-path "sudo -u railinfo rsync" target/universal/railinfo-1.0-SNAPSHOT.zip "${host}:/home/railinfo/" || exit 1
-ssh "${host}" "sudo systemctl stop railinfo"
-ssh "${host}" "sudo -u railinfo -i sh -c 'rm -rf railinfo-1.0-SNAPSHOT; unzip railinfo-1.0-SNAPSHOT.zip'"
-ssh "${host}" "sudo systemctl start railinfo"
+rsync -v -e "ssh -J backup0.kabelverhau.ch" --rsync-path "sudo -u railinfo rsync" target/universal/railinfo-1.0-SNAPSHOT.zip "${host}:/home/railinfo/" || exit 1
+ssh "${host}" -J backup0.kabelverhau.ch "sudo systemctl stop railinfo"
+ssh "${host}" -J backup0.kabelverhau.ch "sudo -u railinfo -i sh -c 'rm -rf railinfo-1.0-SNAPSHOT; unzip railinfo-1.0-SNAPSHOT.zip'"
+ssh "${host}" -J backup0.kabelverhau.ch "sudo systemctl start railinfo"
